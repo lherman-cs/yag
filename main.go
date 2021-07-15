0f84de77f645088ee033effc31a466862c5ca9fe
Lukas Herman <lherman.cs@gmail.com>
2021-07-15 13:43:09 -0700

    Handle closed pipe

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"compress/gzip"
+	"fmt"
 	"io"
 	"os"
 	"os/signal"
@@ -9,13 +11,39 @@ import (
 
 func main() {
 	c := make(chan os.Signal, 1)
+	signal.Ignore(os.Interrupt)
 	signal.Notify(c, os.Interrupt)
-
-	go func() {
-		<-c
-		os.Stdin.Close()
-	}()
-
 	gwriter := gzip.NewWriter(os.Stdout)
-	io.Copy(gwriter, os.Stdin)
+	buffStdin := bufio.NewReader(os.Stdin)
+	eof := false
+
+	buf := make([]byte, 1024)
+	for {
+		select {
+		case <-c:
+			fmt.Fprintln(os.Stderr, "Detected an interrupt signal")
+		default:
+		}
+
+		n, err := buffStdin.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				eof = true
+				fmt.Fprintln(os.Stderr, "Stdin has been closed. Flushing...")
+			} else {
+				fmt.Fprintln(os.Stderr, err)
+				break
+			}
+		}
+
+		if eof {
+			gwriter.Flush()
+			gwriter.Close()
+			os.Stdout.Close()
+			break
+		}
+		gwriter.Write(buf[:n])
+	}
+
+	fmt.Fprintln(os.Stderr, "Compression is done!")
 }
